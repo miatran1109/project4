b4b102b9109426eed57287619bbb24aa9ae94891
MaiTran <miatran1109@gmail.com>
2023-12-10 11:24:14 +0700

    auth0

--- starter/backend/src/lambda/auth/auth0Authorizer.mjs
+++ starter/backend/src/lambda/auth/auth0Authorizer.mjs
@@ -1,10 +1,10 @@
 import Axios from 'axios'
-import jsonwebtoken from 'jsonwebtoken'
+import jsonwebtoken, { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-gnmnuwa1ynu5m7bg.us.auth0.com/.well-known/jwks.json'
 
 export async function handler(event) {
   try {
@@ -46,8 +46,15 @@ async function verifyToken(authHeader) {
   const token = getToken(authHeader)
   const jwt = jsonwebtoken.decode(token, { complete: true })
 
-  // TODO: Implement token verification
-  return undefined;
+  const keys = (await Axios.get(jwksUrl)).data.keys;
+  const signingKey = keys.find(key => key.kid === jwt.header.kid);
+  logger.info('Signing key', { signingKey });
+  if (!signingKey) throw new Error('Invalid signing key');
+  const secret = signingKey.x5c[0];
+  const cert = `-----BEGIN CERTIFICATE----- ${secret} -----END CERTIFICATE-----`
+  const verifiedToken = verify(token, cert, { algorithms: ['RS256'] })
+  logger.info('Token verified', verifiedToken)
+  return verifiedToken;
 }
 
 function getToken(authHeader) {
